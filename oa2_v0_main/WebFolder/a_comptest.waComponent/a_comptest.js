fab87fc7cd0dbc748085a566eb87c0067a03076e
scarpazi <scarpazi@gmail.com>
2014-03-17 17:15:32 +0100

    hangout 20140317
    updated toponym comp; probably fixed problem with populating comboboxes

--- oa2_v0_main/WebFolder/a_comptest.waComponent/a_comptest.js
+++ oa2_v0_main/WebFolder/a_comptest.waComponent/a_comptest.js
@@ -7,27 +7,17 @@ function constructor (id) {
 
 	// @region beginComponentDeclaration// @startlock
 	var $comp = this;
-	this.name = 'toponym-manager';
+	this.name = 'a_comptest';
 	// @endregion// @endlock
 
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
-	var tfilter = {};	// @textField
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
-	tfilter.keyup = function tfilter_keyup (event)// @startlock
-	{// @endlock
-		//WAF.sources.component0_toponym.query("name = :1", event.currentTarget.value + "@");
-		var theName = $$(getHtmlId('tfilter')).getValue();
-		$comp.sources.toponym.query('name = :1 order by name', { params: [theName + "*"]});
-
-	};// @lock
-
 	// @region eventManager// @startlock
-	WAF.addListener(this.id + "_tfilter", "keyup", tfilter.keyup, "WAF");
 	// @endregion// @endlock
 
 	};// @lock
