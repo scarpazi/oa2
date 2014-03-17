52bbb4729681e8400e42922a233dafccf6c8261b
lukdal <luca.daltilia@gmail.com>
2014-03-17 16:58:04 +0100

    Update #74

---
+++ oa2_v0_main/WebFolder/BRMS3.waPage/scripts/index.js
@@ -0,0 +1,32 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button5 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button5.click = function button5_click (event)// @startlock
+	{// @endlock
+		var isChecked = $('#checkbox2').prop('checked');
+
+	};// @lock
+if ($('#checkbox2').is(':checked')) {
+
+   $('#autoForm3').show();
+
+} else {
+
+   $('#autoForm3').hide();
+
+}
+
+};
+
+
+
+// @region eventManager// @startlock
+	WAF.addListener("button5", "click", button5.click, "WAF");
+// @endregion
+};// @endlock
