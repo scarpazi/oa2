bdf1c6ea5ae581d0d818bff56891a04855b9143b
lukdal <luca.daltilia@gmail.com>
2014-04-28 16:01:38 +0200

    Update #75

---
+++ oa2_v0_main/WebFolder/BRMS10.waPage/scripts/index.js
@@ -0,0 +1,45 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button1 = {};	// @button
+	var button2 = {};	// @button
+	var button9 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button1.click = function button1_click (event)// @startlock
+	{// @endlock
+		   $('#autoForm1').show();
+
+	};// @lock
+
+	button2.click = function button2_click (event)// @startlock
+	{// @endlock
+   $('#richText1').toggle();
+   $('#richText2').toggle();
+
+
+	};// @lock
+
+	button9.click = function button9_click (event)// @startlock
+	{// @endlock
+		if ($bibliographic_reference.Journal && $bibliographic_reference.volume)
+            { $$(getHtmlId('autoForm3')).hide();
+              $$(getHtmlId('autoForm8')).hide();
+              $$(getHtmlId('autoForm7')).show();}
+        else
+        if ($bibliographic_reference.Journal)
+			{ $$(getHtmlId('autoForm7')).hide();
+              $$(getHtmlId('autoForm8')).hide();
+              $$(getHtmlId('autoForm3')).show();};
+
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button1", "click", button1.click, "WAF");
+	WAF.addListener("button2", "click", button2.click, "WAF");
+	WAF.addListener("button9", "click", button9.click, "WAF");
+// @endregion
+};// @endlock
