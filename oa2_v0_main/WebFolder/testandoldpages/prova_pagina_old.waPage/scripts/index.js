4dfc3ffdbf34c2d96d3d07ecf89e5af178b10fcb
scarpazi <WakandaDeveloper@wakanda.org>
2014-06-05 00:41:49 +0200

    post-merge fixes #4

---
+++ oa2_v0_main/WebFolder/testandoldpages/prova_pagina_old.waPage/scripts/index.js
@@ -0,0 +1,25 @@
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var search = {};	// @button
+	var livequery = {};	// @textField
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	search.click = function search_click (event)// @startlock
+	{// @endlock
+		sources.site.query('name = :1 order by name', {params : [queryStr + '*']});
+	};// @lock
+
+	livequery.keyup = function livequery_keyup (event)// @startlock
+	{// @endlock
+		var theName = $$("livequery").getValue();
+		sources.site.query('name = :1 order by name', { params: [theName + "*"]});
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("search", "click", search.click, "WAF");
+	WAF.addListener("livequery", "keyup", livequery.keyup, "WAF");
+// @endregion
+};// @endlock
